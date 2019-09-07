b3f08c014d0815aed658bdea70a6aaacd6689ccd
liyu <liyu@ghcchina.com.cn>
2019-09-07 15:16:02 +0800

    testtesttest

---
+++ build/lib/fspider/test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Frank Li'
+
+def my_print():
+    print('just a test pypi upload...')
+
+if __name__ == '__main__':
+    my_print()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='frankspider',
+    version='1.0.0',
+    description='a spider tool',
+    author='frank',
+    author_email='frank@gmail.com',
+    url='https://github.com/the0demiurge/ShadowSocksShare',
+    #packages=find_packages(),
+    packages=['fspider'],
+    #install_requires=['requests'],
+)
